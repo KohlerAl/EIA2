21a296fb7f0b2ad91322ce66762c8da154fa52f1
KohlerAl <56314717+KohlerAl@users.noreply.github.com>
2020-05-02 17:00:18 +0200

    Ergänzung Aufgabe 2

--- L02_Events/Sequenzmemory_Hannah/Hannah_Memory.js
+++ L02_Events/Sequenzmemory_Hannah/Hannah_Memory.js
@@ -3,8 +3,8 @@ var L02_Sequenzmemory;
 (function (L02_Sequenzmemory) {
     /**
      * Der Aufbau mit den Events ist nicht ganz richtig. Keine Funktion wird irgendwo aufgerufen.
-     * Ich hab das ganze mal "interpretiert", damit ich wenigstens schauen kann, ob der Aufbau
-     * grob hinaut
+     * Ich hab das ganze mal so "interpretiert" wie ich denke, dass es gemeint war,
+     *  damit ich wenigstens schauen kann, ob der Aufbau grob hinaut
      */
     let word = document.getElementById("selectPredefined");
     let playground = document.getElementById("playground");
--- L02_Events/Sequenzmemory_Hannah/Hannah_Memory.ts
+++ L02_Events/Sequenzmemory_Hannah/Hannah_Memory.ts
@@ -2,8 +2,8 @@ namespace L02_Sequenzmemory {
     
     /**
      * Der Aufbau mit den Events ist nicht ganz richtig. Keine Funktion wird irgendwo aufgerufen.
-     * Ich hab das ganze mal "interpretiert", damit ich wenigstens schauen kann, ob der Aufbau
-     * grob hinaut
+     * Ich hab das ganze mal so "interpretiert" wie ich denke, dass es gemeint war,
+     *  damit ich wenigstens schauen kann, ob der Aufbau grob hinaut
      */
 
     let word : HTMLInputElement = <HTMLInputElement>document.getElementById("selectPredefined"); 
