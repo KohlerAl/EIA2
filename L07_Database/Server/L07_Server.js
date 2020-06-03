c6d31e3501d7089b0de28b064181f7bfcd246afe
KohlerAl <56314717+KohlerAl@users.noreply.github.com>
2020-06-03 11:34:03 +0200

    Test

--- L07_Database/Server/L07_Server.js
+++ L07_Database/Server/L07_Server.js
@@ -32,6 +32,7 @@ var L07_Household;
         console.log("What's up?");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
+        console.log("Request-URL:  " + _request.url);
         if (_request.url && _request.url != "") {
             let url = Url.parse(_request.url, true);
             /* for (let key in url.query) {
