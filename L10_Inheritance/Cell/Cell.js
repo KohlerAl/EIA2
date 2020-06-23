96495a343029609d839aa921c3897d7f43c1fe6e
KohlerAl <56314717+KohlerAl@users.noreply.github.com>
2020-06-23 16:06:08 +0200

    Add Files for Cell and added link in html

---
+++ L10_Inheritance/Cell/Cell.js
@@ -0,0 +1,8 @@
+"use strict";
+var L10_Virus;
+(function (L10_Virus) {
+    class Cell {
+    }
+    L10_Virus.Cell = Cell;
+})(L10_Virus || (L10_Virus = {}));
+//# sourceMappingURL=Cell.js.map
---
+++ L10_Inheritance/Cell/Cell.ts
@@ -0,0 +1,5 @@
+namespace L10_Virus {
+    export class Cell {
+        
+    }
+}
